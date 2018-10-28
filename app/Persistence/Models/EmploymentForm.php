aa7e291d42dda3c3b3d95f5eaa3a8085c857b98a
G09 <info@viraggabor.com>
2018-10-28 12:54:30 +0100

    EmploymentFormmodel

---
+++ app/Persistence/Models/EmploymentForm.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: g09
+ * Date: 2018.10.28.
+ * Time: 12:53
+ */
+
+namespace App\Persistence\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class EmploymentForm extends Model
+{
+
+    protected $primaryKey = 'id_employment_form';
+
+    protected $fillable = 'name';
+
+}
