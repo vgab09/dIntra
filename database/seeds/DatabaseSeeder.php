86a2d7881e1e9506f0f7dd0a741b26e5af640432
G09 <info@viraggabor.com>
2018-11-01 17:41:38 +0100

    default seeder 1/2

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Persistence\Models\Employee;
+use App\Persistence\Models\EmploymentForm;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +15,47 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->addRoles();
+        $this->addPermissions();
+        $this->addEmploymentForms();
+        $this->addAdministrator();
+        $this->addConstantHollidays();
+
     }
+
+    private function addEmploymentForms()
+    {
+
+        factory(EmploymentForm::class)->create(['name' => 'Teljes munkaidős']);
+        factory(EmploymentForm::class)->create(['name' => 'Részmunkaidős']);
+        factory(EmploymentForm::class)->create(['name' => 'Szerződéses']);
+        factory(EmploymentForm::class)->create(['name' => 'Ideiglenes']);
+        factory(EmploymentForm::class)->create(['name' => 'Gyakornok']);
+
+    }
+
+    private function addAdministrator()
+    {
+        factory(Employee::class)->create(['name' => 'Administrator', 'hiring_date' => Carbon::now(), 'termination_date' => null, 'email' => 'web@erppartner.hu', 'active' => 1]);
+    }
+
+    private function addRoles()
+    {
+        $adminRole = Role::create(['name' => 'Adminisztrátor']);
+        $managerRole = Role::create(['name' => 'Manager']);
+        $leader = Role::create(['name' => 'Vezető']);
+        $employeeRole = Role::create(['name' => 'Munkatárs']);
+    }
+
+    private function addPermissions()
+    {
+
+    }
+
+    private function addConstantHollidays()
+    {
+
+    }
+
+
 }
