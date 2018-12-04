398e0dab0e698299771129a2e5be7263608afc26
G09 <info@viraggabor.com>
2018-12-04 20:34:23 +0100

    logout route javítva

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@
 // Authentication Routes...
 $this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
 $this->post('login', 'Auth\LoginController@login');
-$this->post('logout', 'Auth\LoginController@logout')->name('logout');
+$this->get('logout', 'Auth\LoginController@logout')->name('logout');
 
 // Password Reset Routes...
 $this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
