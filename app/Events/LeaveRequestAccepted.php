be3adaf24227fbc45ac55c9356860ccbc57b86e8
G09 <info@viraggabor.com>
2019-03-08 16:56:08 +0100

    Szabadság igényekkel kapcsolatos event-ek

---
+++ app/Events/LeaveRequestAccepted.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Events;
+
+use App\Persistence\Models\Employee;
+use App\Persistence\Models\LeaveRequest;
+use App\Persistence\Models\LeaveRequestHistory;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class LeaveRequestAccepted
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    protected $leaveRequest;
+    protected $history;
+    protected $perimeter;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param LeaveRequest $leaveRequest
+     * @param LeaveRequestHistory $history
+     * @param Employee $perimeter
+     */
+    public function __construct(LeaveRequest $leaveRequest,LeaveRequestHistory $history, Employee $perimeter)
+    {
+        $this->leaveRequest = $leaveRequest;
+        $this->history = $history;
+        $this->perimeter = $perimeter;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
