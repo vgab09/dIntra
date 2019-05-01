7b38c409e22f31b06db7557b61c6f9a1c5bd59e6
gabor.virag <info@viraggabor.com>
2019-05-01 15:19:32 +0200

    css generálás

---
+++ resources/lang/hu/messages.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    /**
+     * Password reset email translations.
+     */
+    'You are receiving this email because we received a password reset request for your account.' => 'Ezt az emailt azért küldtük Önnek, mert egy jelszó visszaállítási kérést kaptunk az Ön fiókjával kapcsolatban.',
+    'Reset Password' => 'Jelszó visszaállítása',
+    'If you did not request a password reset, no further action is required.' => 'Ha nem Ön kérte a jelszó visszaállítását, nincs egyéb teendője.',
+    'If you’re having trouble clicking the button, copy and paste the URL below into your web browser:' => 'Ha probléma adódik a(z) ":actionText" gomb megnyomásakor, próbálja meg az alábbi linket bemásolni a böngésző címsorába:',
+];
